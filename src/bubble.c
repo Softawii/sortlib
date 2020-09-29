661a1d7a92654481dcab01f8d042cebea5e9b4b0
Yan Figueiredo <yancfm@gmail.com>
2020-09-29 16:28:26 -0300

    bubble

---
+++ src/bubble.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * void *vector: a pointer to the first element of a vector.
+ * int n: number of vector elements.
+ * int size: size in bytes of a element.
+ * int cmp: pointer to a comparative function.
+ */
+void bubblesort(void *vector, int n, int size, int (*cmp)(const void*, const void*)) {
+
+}
