49278ecdc5daf1845f2e06281b5b5083c9cc576b
Yan Figueiredo <49878392+yaansz@users.noreply.github.com>
2020-10-01 18:17:46 -0300

    Update merge.h
    a função `merge` não é pública. Logo, não necessita estar neste arquivo n?

--- src/merge.h
+++ src/merge.h
@@ -2,7 +2,7 @@
 #define SORTLIB_MERGE_H
 
 /**
- * Sort a vector using insertion sort algorithm.
+ * Sort a vector using merge sort algorithm.
  *
  * @param vector        Pointer to the vector to be sorted.
  * @param n             Vector size in elements.
@@ -12,17 +12,5 @@
  * @return              void.
  */
 extern void mergesort(void *vector, int n, int size, int (*compare)(void*,  void*));
-
-/**
- * Sort a vector using insertion sort algorithm.
- *
- * @param vector        Pointer to the vector to be sorted.
- * @param n             Vector size in elements.
- * @param size          Element size in bytes.
- * @param compare       Pointer to the function that
- *                      compares the vector elements.
- * @return              void.
- */
-void merge(void *vector, int n, int size, int (*compare)(void*,  void*));
 
 #endif
